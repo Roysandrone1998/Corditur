f96d219974a326ccd78608c76e38622ac2d57522
Roysandrone1998 <raysandrone@gmail.com>
2025-11-18 00:03:21 -0300

    capazs

--- turismo-admin/src/api/axios.js
+++ turismo-admin/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000/api',
+  baseURL: import.meta.env.VITE_API_URL || '/api',
   credentials: true // 👈 para enviar/recibir la cookie
 });
 api.interceptors.request.use((config) => {
